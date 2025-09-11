f0bdabca948b39e8128e7880692ee06ec6ea7e2e
Alena Kislenko <alena.golovleva@mail.ru>
2025-09-11 19:38:40 +0700

    Добавлены функции обработки данных

---
+++ src/processing.py
@@ -0,0 +1,8 @@
+def filter_by_state(data, state ='EXECUTED'):
+    """Функция фильтрует список словарей по значению ключа 'state'"""
+    return [item for item in data if item['state']==state]
+
+
+def sort_by_date(dats, descending = True):
+    """Функция сортирует список словарей по ключу 'date'"""
+    return sorted(data, key = lambda x: x['date'], reverse= descending)
